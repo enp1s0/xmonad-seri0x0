d5c369b740dcfd8d5ba7e9642e680e26178db829
mutsuki <mutsuki@momo86.net>
2019-12-12 14:19:42 +0900

    Fix window size of FileChooserDialog

--- xmonad.hs
+++ xmonad.hs
@@ -1,7 +1,10 @@
 import XMonad
+import qualified XMonad.StackSet as W
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers (doRectFloat)
 import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.WindowProperties (Property (..), propertyToQuery)
 import XMonad.Util.EZConfig
 import System.IO
 
@@ -14,7 +17,7 @@ main = do
         , normalBorderColor = "#11103B"
         , focusedBorderColor = "#D4D4D6"
         , startupHook = spawn "feh --bg-fill ~/images/xmonad.jpg"
-        , manageHook = manageDocks <+> manageHook defaultConfig
+        , manageHook = myManageHook
         , layoutHook = avoidStruts $ layoutHook defaultConfig
         , logHook = dynamicLogWithPP xmobarPP
                 { ppOutput = hPutStrLn xmproc
@@ -22,6 +25,15 @@ main = do
                 }
         , handleEventHook = docksEventHook
         } `additionalKeys` myAdditionalKeys
+
+myManageHook = composeAll
+    [ propertyToQuery (Role "GtkFileChooserDialog")
+        --> doRectFloat (W.RationalRect 0.25 0.25 0.5 0.5)
+    ]
+    <+>
+    manageDocks
+    <+>
+    manageHook defaultConfig
 
 myAdditionalKeys = [
     ((mod4Mask .|. shiftMask, xK_w), spawn "chromium"),
